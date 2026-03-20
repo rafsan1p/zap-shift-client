fdc8537123e751dcb8aea14c42f15b25c1ef782c
rafsan1p <rubayetrafsan605@gmail.com>
2026-03-20 21:14:23 +0600

    search location in the map to fly

--- src/pages/Coverage/Coverage.jsx
+++ src/pages/Coverage/Coverage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import { useLoaderData } from 'react-router';
@@ -6,14 +6,27 @@ import { useLoaderData } from 'react-router';
 const Coverage = () => {
     const position = [23.6850, 90.3563]
     const serviceCenters = useLoaderData();
-    console.log(serviceCenters);
+    //console.log(serviceCenters);
+    const mapRef = useRef(null);
+
+    const handleSearch = e =>{
+        e.preventDefault();
+        const location = e.target.location.value;
+        const district = serviceCenters.find(c => c.district.toLowerCase().includes(location.toLowerCase()));
+        if(district){
+            const coord = [district.latitude, district.longitude];
+            console.log(district, coord);
+            //go to the location
+            mapRef.current.flyTo(coord, 14);
+        }
+    }
 
     return (
         <div className='sm:px-2 md:p-20'>
             <h2 className="sm:text-2xl md:text-5xl font-extrabold mb-7">We are available in 64 districts</h2>
             <div className='my- 7'>
                 {/* Search */}
-                <form>
+                <form onSubmit={handleSearch}>
                     <label className="input">
                         <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                             <g
@@ -27,7 +40,7 @@ const Coverage = () => {
                                 <path d="m21 21-4.3-4.3"></path>
                             </g>
                         </svg>
-                        <input type="search" className="grow" placeholder="Search" />
+                        <input type="search" className="grow" name="location" placeholder="Search" />
                     </label>
                 </form>
             </div>
@@ -38,6 +51,7 @@ const Coverage = () => {
                     zoom={8}
                     scrollWheelZoom={false}
                     className='h-200'
+                    ref={mapRef}
                 >
                     <TileLayer
                         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
