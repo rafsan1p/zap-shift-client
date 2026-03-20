d41b86b69e5a5edf43105f62135453af88450ffa
rafsan1p <rubayetrafsan605@gmail.com>
2026-03-20 09:06:54 +0600

    customize theme

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -3,8 +3,9 @@ import logo from '../../assets/logo.png'
 
 const Logo = () => {
     return (
-        <div>
+        <div className='flex items-end'>
             <img src={logo} alt="" />
+            <h3 className="text-3xl font-bold -ms-2.5">zapShift</h3>
         </div>
     );
 };
